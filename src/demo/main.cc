0bb0f11e9631e07d4934af65de42972031a93ecb
Matan Nassau <matan.nassau@gmail.com>
2008-11-10 01:17:40 -0500

    initial commit of lexical analysis playground
    ws() consumes whitespace, maintaining line_number while at it if any of
    the whitespace consumed are newlines.

--- src/demo/main.cc
+++ src/demo/main.cc
@@ -1,45 +1,24 @@
-#include <yaml++.hh>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-template<typename T>
-string dump(T t)
-{
-    cerr << "FATAL:Don't call the generic template." << endl;
-    exit(-1);
-}
+int line_number = 1;
 
-template<>
-string dump<string>(string str)
-{
-    return str;
-}
-
-template<>
-string dump<vector<string> >(vector<string> vec)
-{
-    string result;
-    for( vector<string>::const_iterator iter(vec.begin());
-        iter != vec.end();
-        ++iter ) {
-        result += "- ";
-        result += dump(*iter);
-        result += "\n";
+void ws() {
+    while( true ) {
+        if( ! cin ) return;
+        if( cin.peek() == '\n' )
+            ++line_number;
+        else if( cin.peek() != ' ' && cin.peek() != '\t' )
+            return;
+        cin.get();
     }
-    return result;
 }
 
 int main(int argc, char* argv[])
 {
-    vector<string> v;
-    v.push_back("a");
-    v.push_back("b");
-    v.push_back("c");
-    cout << dump(v) << endl;
-
+    ws();
+    char ch = cin.peek();
+    cout << ch << line_number << endl;
     return 0;
 }
