059b48e14030ed830d841f6f05f014b8de2ed2f0
Matan Nassau <matan.nassau@gmail.com>
2008-11-13 02:42:17 -0500

    formatted token names in token class

--- src/token.hh
+++ src/token.hh
@@ -4,19 +4,32 @@
 class token {
 public:
     enum lexical_tag {
-        ALIAS,                        ANCHOR,
-        BLOCK_SEQUENCE_ELEMENT_BEGIN, COMPLEX_MAPPING_KEY,
-        DOCUMENT_BEGIN,               DOCUMENT_END,
-        ESCAPE_SEQUENCE,              FLOW_MAPPING_END,
-        FLOW_MAPPING_BEGIN,           FLOW_SEQUENCE_END,
-        FLOW_SEQUENCE_BEGIN,          FOLDED_STYLE,
-        HANDLE,                       IDENTIFIER,
-        INTEGER,                      LITERAL_STYLE,
-        LOCAL_TAG,                    NIL,
-        PAIR_SEPARATOR,               STANDARD_TAG,
-        STRING,                       TAG,
-        TAG_DIRECTIVE,                URI,
-        VERSION_DIRECTIVE,            EOS
+        ALIAS,
+        ANCHOR,
+        BLOCK_SEQUENCE_ELEMENT_BEGIN,
+        COMPLEX_MAPPING_KEY,
+        DOCUMENT_BEGIN,
+        DOCUMENT_END,
+        EOS,
+        ESCAPE_SEQUENCE,
+        FLOW_MAPPING_BEGIN,
+        FLOW_MAPPING_END,
+        FLOW_SEQUENCE_BEGIN,
+        FLOW_SEQUENCE_END,
+        FOLDED_STYLE,
+        HANDLE,
+        IDENTIFIER,
+        INTEGER,
+        LITERAL_STYLE,
+        LOCAL_TAG,
+        NIL,
+        PAIR_SEPARATOR,
+        STANDARD_TAG,
+        STRING,
+        TAG,
+        TAG_DIRECTIVE,
+        URI,
+        VERSION_DIRECTIVE
     };
 
     token();
