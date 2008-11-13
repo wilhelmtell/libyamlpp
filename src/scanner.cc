845f73d1314085c9442b21a3a507e45ea2550956
Matan Nassau <matan.nassau@gmail.com>
2008-11-13 01:45:23 -0500

    scanner:  added sip(), input buffer and prev token
    sip() takes input from input stream, and places it in the input buffer.
    
    previous token gives more context by storing the previous token emitted.
    looking at the yaml language, i think sometimes a token's name depends
    on the previous token emitted.  for example: "-\n", a dash followed by a
    newline, should parse to [null], an array with a null element.  so, the
    newline parses into a null.  a newline, therefore, may translate to
    various tokens depending on the previous token...

--- src/scanner.cc
+++ src/scanner.cc
@@ -1,5 +1,7 @@
 #include "scanner.hh"
+#include <cctype>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -7,13 +9,33 @@ scanner::scanner() : is(cin), line_number(1), peek(' ')
 {
 }
 
+// Fill peek with the next character from the input stream
+void scanner::sip()
+{
+    // maintain a buffer so we do io less often
+    const queue<char>::size_type BUFFER_SIZE = 4096;
+    if( buf.empty() )
+        for( queue<char>::size_type i = 0; i < BUFFER_SIZE; ++i ) {
+            char ch = is.get();
+            if( ! is ) {
+                buf.push(-1);
+                break;
+            }
+            buf.push(ch);
+        }
+    peek = buf.front();
+    buf.pop();
+    assert(buf.size() <= BUFFER_SIZE);
+}
+
 token scanner::scan()
 {
     // skip whitespace
+    // TODO:  wrong.  we need to account for indentation.
     while( true ) {
         if( peek == '\n' )
             ++line_number;
-        else if( peek != ' ' && peek != '\t' )
+        else if( ! isspace(peek) )
             break;
         peek = is.get();
         if( ! is ) break;
--- src/scanner.hh
+++ src/scanner.hh
@@ -2,17 +2,23 @@
 #define SRC_SCANNER_HH_
 
 #include "token.hh"
+#include <queue>
+#include <stack>
 #include <iosfwd>
 
 class scanner {
 public:
     scanner();
     token scan();
+    void sip();
 
 private:
     std::istream &is;
     int line_number;
     char peek;
+    std::queue<char> buf;
+    token previous;
+    std::stack<int> indentation_level;
 };
 
 #endif // SRC_SCANNER_HH_
