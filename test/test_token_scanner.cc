78c43049e8030818da15a064a18492c6704ce0a5
Matan Nassau <matan.nassau@gmail.com>
2008-11-18 16:04:47 -0500

    updated test_token_scanner

--- test/test_token_scanner.cc
+++ test/test_token_scanner.cc
@@ -9,11 +9,25 @@ using namespace std;
 TEST(TokenScanner, CanScanStream)
 {
     stringstream ss("[");
-    presentation_input* pi = new presentation_input(ss);
-    token_scanner* s2 = new flow_sequence_begin_scanner(pi, 0);
-    token_scanner* s1 = new flow_sequence_begin_scanner(pi, s2);
+    presentation_input* input = new presentation_input(ss);
+    token_scanner* s2 = new flow_sequence_begin_scanner(0);
+    token_scanner* s1 = new flow_sequence_begin_scanner(s2);
 
     // TODO:  s1, s2 should be of different classes.
     // we have only one subclass of token_scanner right now.
-    EXPECT_TRUE(s1->scan());
+    EXPECT_TRUE(s1->scan(input));
+}
+
+TEST(TokenScanner, ScanSequenceBegin)
+{
+    stringstream ss("[");
+    presentation_input* input = new presentation_input(ss);
+    token_scanner* s2 = new flow_sequence_begin_scanner(0);
+    token_scanner* s1 = new flow_sequence_begin_scanner(s2);
+    s1->scan(input);
+    token result(s1->previous());
+
+    // TODO:  s1, s2 should be of different classes.
+    // we have only one subclass of token_scanner right now.
+    EXPECT_EQ(token::FLOW_SEQUENCE_BEGIN, result.tag);
 }
