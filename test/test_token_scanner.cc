89ace89eddbb11d5cb85361a861859806eddea3b
Matan Nassau <matan.nassau@gmail.com>
2008-11-18 14:00:23 -0500

    update scanner test:  use successor, not find_if

--- test/test_token_scanner.cc
+++ test/test_token_scanner.cc
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <algorithm>
 #include <sstream>
 #include "../src/token_scanner.hh"
 #include "../src/flow_sequence_begin_scanner.hh"
@@ -7,17 +6,14 @@
 
 using namespace std;
 
-TEST(TokenScanner, ScanStream)
+TEST(TokenScanner, CanScanStream)
 {
     stringstream ss("[");
     presentation_input* pi = new presentation_input(ss);
-    token_scanner* s1 = new flow_sequence_begin_scanner(pi);
-    token_scanner* s2 = new flow_sequence_begin_scanner(pi);
+    token_scanner* s2 = new flow_sequence_begin_scanner(pi, 0);
+    token_scanner* s1 = new flow_sequence_begin_scanner(pi, s2);
 
     // TODO:  s1, s2 should be of different classes.
     // we have only one subclass of token_scanner right now.
-    token_scanner* scanners[] = {s1, s2};
-    token_scanner** p = find_if(scanners, scanners+2,
-                                mem_fun(&token_scanner::recognize));
-    EXPECT_EQ(*(scanners), *p);
+    EXPECT_TRUE(s1->scan());
 }
