72590809130617970cf29e3ef4df390ec9a4772c
Matan Nassau <matan.nassau@gmail.com>
2008-11-19 21:15:11 -0500

    added a one-command build script
    build.sh grabs Boost.Build, sets it up and finally builds the project.
    the script builds the library as well as the demo application, and it
    also performs unit tests.

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###############################################################################
+# Build libyaml++.  Grabs and sets up Boost.Build if necessary.
+#
+# Requires:  bash, wget, tar, rm, grep
+###############################################################################
+
+if [ \! -d boost-build ]; then
+  echo "Downloading Boost.Build ..."
+  wget -q http://prdownloads.sourceforge.net/boost/boost-build-2.0-m12.tar.bz2
+  echo "Extracting Boost.Build ..."
+  tar jxf boost-build-2.0-m12.tar.bz2
+  rm -f boost-build-2.0-m12.tar.bz2
+  cd boost-build/jam_src
+  echo "Building Boost.Build ..."
+  ./build.sh >/dev/null 2>&1
+  cd - >/dev/null 2>&1
+  echo "Configuring Boost.Build ..."
+fi
+grep -q '^using gcc ;' boost-build/user-config.jam
+
+for TOOL in gcc doxygen ; do
+  grep -q "^using $TOOL ;" boost-build/user-config.jam
+  if [ $? -ne 0 ]; then
+    echo "using $TOOL ;" >> boost-build/user-config.jam
+  fi
+done
+
+if [ -z "$BOOST_BUILD_PATH" ]; then
+  export BOOST_BUILD_PATH="$(pwd)/boost-build"
+fi
+
+echo "Building libyaml++ ..."
+(boost-build/jam_src/bin.linuxx86/bjam libyaml++ check demo) >/dev/null 2>&1
+echo
+echo "###############################################################################"
+echo "# DONE KTHXBAI                                                                #"
+echo "###############################################################################"
