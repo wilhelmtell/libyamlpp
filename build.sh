05955fe4ec30341363634b42b8d010fc3be6910d
Matan Nassau <matan.nassau@gmail.com>
2008-11-19 23:47:46 -0500

    added success/failure messages to buid.sh

--- build.sh
+++ build.sh
@@ -2,36 +2,45 @@
 ###############################################################################
 # Build libyaml++.  Grabs and sets up Boost.Build if necessary.
 #
-# Requires:  bash, wget, tar, rm, grep
+# Requires:  wget, bash, gnu core-utils
+#            Unless Boost.Build and GoogleTest are installed, an internet
+#            connection is required as well.
 ###############################################################################
 
-if [ \! -d boost-build ]; then
-  echo "Downloading Boost.Build ..."
-  wget -q http://prdownloads.sourceforge.net/boost/boost-build-2.0-m12.tar.bz2
-  echo "Extracting Boost.Build ..."
-  tar jxf boost-build-2.0-m12.tar.bz2
+fail() {
+  echo -e "\033[0;31mFAIL\033[0m"
+  exit 1;
+}
+
+succ() {
+  echo -e "\033[0;32mOK\033[0m"
+}
+
+which bjam >/dev/null 2>&1
+if [ $? -ne 0 -o \! -d boost-build ]; then
+  echo -n "Downloading Boost.Build ...  "
+  wget -q http://prdownloads.sourceforge.net/boost/boost-build-2.0-m12.tar.bz2 && succ || fail
+  echo -n "Extracting Boost.Build ...  "
+  tar jxf boost-build-2.0-m12.tar.bz2 && succ || fail
   rm -f boost-build-2.0-m12.tar.bz2
   cd boost-build/jam_src
-  echo "Building Boost.Build ..."
-  ./build.sh >/dev/null 2>&1
+  echo -n "Building Boost.Jam ...  "
+  ./build.sh >/dev/null 2>&1 && succ || fail
   cd - >/dev/null 2>&1
 fi
-
-echo "Configuring Boost.Build ..."
-for TOOL in gcc doxygen ; do
-  grep -q "^using $TOOL ;" boost-build/user-config.jam
-  if [ $? -ne 0 ]; then
-    echo "using $TOOL ;" >> boost-build/user-config.jam
-  fi
-done
 
 if [ -z "$BOOST_BUILD_PATH" ]; then
   export BOOST_BUILD_PATH="$(pwd)/boost-build"
 fi
 
-echo "Building libyaml++ ..."
-(boost-build/jam_src/bin.linuxx86/bjam libyaml++ check demo) >/dev/null 2>&1
-echo
-echo "###############################################################################"
-echo "# DONE KTHXBAI                                                                #"
-echo "###############################################################################"
+for TOOL in gcc doxygen ; do
+  grep -q "^using $TOOL ;" boost-build/user-config.jam
+  if [ $? -ne 0 ]; then
+    echo -n "Configuring Boost.Build for $TOOL ...  "
+    echo "using $TOOL ;" >> boost-build/user-config.jam && succ || fail
+  fi
+done
+
+echo -n "Building libyaml++ ...  "
+(boost-build/jam_src/bin.linuxx86/bjam libyaml++ check demo) >/dev/null 2>&1 && succ || fail
+echo -e "\nDone."
