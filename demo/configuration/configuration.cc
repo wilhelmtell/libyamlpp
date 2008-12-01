e252d83c1cbce8d4b35b7838977895c8376623e3
Matan Nassau <matan.nassau@gmail.com>
2008-11-30 23:49:40 -0500

    allow input from stdin in configuration demo
    if configuration demo is invoked with no command-line argument or with
    argument "-", input is taken from stdin.  Otherwise the first
    command-line argument is taken as the name of the file to open for
    parsing.
    
    This implies I had to take off the usage message.  Instead, I placed it
    in the source file.  I think this is fine because the whole point of
    this is demonstrating libyaml++, so the source file will be read anyhow.

--- demo/configuration/configuration.cc
+++ demo/configuration/configuration.cc
@@ -6,27 +6,39 @@
 #include <stdexcept>
 #include <string>
 
+///////////////////////////////////////////////////////////////////////////////
+// Usage:  demo-configuration [conf_file]
+//
+// If conf_file is not given or if conf_file is "-" then configuration input is
+// taken from stdin.
+//
+// Try passing in configuration.yaml, supplied in the directory of the source
+// files of this binary.
+///////////////////////////////////////////////////////////////////////////////
+
 using namespace std;
+
+void process(istream& in)
+{
+    tr1::shared_ptr<yaml_handler> handler(new yaml_handler());
+    if( ! in )
+        throw runtime_error("Error:  Can't open configuration stream.");
+    yaml::load(in, handler);
+}
 
 int main(int argc, char* argv[])
 {
-    if( argc < 2 ) {
-        cout << "Usage:  " << argv[0] << " <conf-file>" << endl;
-        cout << endl;
-        cout << "Try passing in configuration.yaml, supplied in the" << endl;
-        cout << "directory of the source files of this binary." << endl;
-        return 1;
-    }
-
-    tr1::shared_ptr<yaml_handler> handler(new yaml_handler());
-    ifstream configuration_file(argv[1]);
-    try {
-        if( ! configuration_file.is_open() )
-            throw runtime_error("Error:  Can't open configration file.");
-        yaml::load(configuration_file, handler); // throws on syntax error
+    // open file if user supplied one in argv[1], stdin otherwise.
+    try { // throws on syntax error or file-not-found
+        if( argc < 2 || string(argv[1]) == "-" ) process(cin);
+        else {
+            ifstream configuration_file(argv[1]);
+            process(configuration_file);
+        }
     }
     catch( const runtime_error& e ) {
         cerr << e.what() << endl;
+        return 1;
     }
 
     return 0;
